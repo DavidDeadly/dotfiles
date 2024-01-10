4fb1e331a3e599877526fafcf6841d8d4f4fc94d
DavidDeadly <jdrueda513@gmail.com>
2024-01-10 00:15:48 -0500

    eww: add player-selector dialog

--- eww/widgets/player-manager/scripts/mediaplayer.py
+++ eww/widgets/player-manager/scripts/mediaplayer.py
@@ -21,7 +21,8 @@ class PlayerManager:
     default_player = "none"
     default_data = {
         "name": "No player",
-        "status": "None"
+        "status": "None",
+        "artist-song": "Nothing playing"
     }
     players_data = {}
 
@@ -59,6 +60,7 @@ class PlayerManager:
             self.update_player(first_playing_player.props.player_name)
 
         self.write_output()
+        self.set_players_list()
 
     def init_player(self, player_data):
         player = Playerctl.Player.new_from_name(player_data)
@@ -66,8 +68,10 @@ class PlayerManager:
         self.register_player(player)
 
         player.connect("playback-status", self.on_playback_status_changed, None)
+        player.connect("metadata", self.on_metadata_changed, None)
 
         self.manager.manage_player(player)
+        self.on_metadata_changed(player, player.props.metadata)
 
     def register_player(self, player):
         player_status = self.parse_status(player.props.playback_status)
@@ -76,9 +80,12 @@ class PlayerManager:
             "name": player.props.player_name,
             "status": player_status
         }
+    
+    def get_players(self):
+        return self.manager.props.players
 
     def get_first_playing_player(self):
-        players = self.manager.props.players
+        players = self.get_players()
 
         if len(players) > 0:
             # if any are playing, show the first one that is playing
@@ -112,6 +119,22 @@ class PlayerManager:
         self.write_output()
         self.update_player(player_name)
 
+    def on_metadata_changed(self, player, metadata, _=None):
+        player_name = player.props.player_name
+        artist = player.get_artist()
+        title = player.get_title()
+
+        track_info = ""
+        if player_name == "spotify" and "mpris:trackid" in metadata.keys() and ":ad:" in player.props.metadata["mpris:trackid"]:
+            track_info = "Advertisement"
+        elif artist is not None and title is not None:
+            track_info = f"{artist} - {title}"
+        else:
+            track_info = title
+
+        self.players_data[player_name]["artist-song"] = track_info
+        self.write_output()
+
     def update_player(self, player_name):
         os.system(f"eww update player={player_name}")
 
@@ -123,6 +146,17 @@ class PlayerManager:
     def write_output(self):
         sys.stdout.write(json.dumps(self.players_data) + "\n")
         sys.stdout.flush()
+
+    def set_players_list(self):
+        players = self.get_players()
+        players_list = list(
+            map(
+                lambda player: player.props.player_name,
+                players
+            )
+        )
+
+        os.system(f"eww update players-list='{players_list}'")
 
 
 def main():
