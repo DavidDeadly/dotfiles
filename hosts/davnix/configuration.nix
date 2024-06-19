cf8d611d34371a1a757e2161e7d3716de29d16ca
DavidDeadly <jdrueda513@gmail.com>
2024-06-19 02:21:55 -0500

    nixos/home-manager: selecte the correct bat

--- hosts/davnix/configuration.nix
+++ hosts/davnix/configuration.nix
@@ -87,11 +87,11 @@
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 50;
+        CPU_MAX_PERF_ON_BAT = 80;
 
         #Optional helps save long term battery health
-        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+        START_CHARGE_THRESH_BAT1 = 40; # 40 and bellow it starts to charge
+        STOP_CHARGE_THRESH_BAT1 = 80; # 80 and above it stops charging
       };
     };
 
