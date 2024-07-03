ab9868dd13732c78a934c9007dd5d09e8b9aad7d
DavidDeadly <jdrueda513@gmail.com>
2024-07-02 19:20:55 -0500

    hyprland: fix select_wallpaper script to use fuzzel instead of wofi

--- .config/hypr/scripts/select_wallpaper.sh
+++ .config/hypr/scripts/select_wallpaper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# cSpell:ignore wofi iname dmenu wholename swww
+# cSpell:ignore fuzzel iname dmenu wholename swww
 
 if [ -z "$WALLPAPERS" ]; then
     printf "Please declare a WALLPAPERS env var like this: \n\t export WALLPAPERS=<directory>\n"
@@ -17,8 +17,7 @@ img=$(
         \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) \
         -not -wholename "$current"\
         -printf "%f\n" \
-        | wofi --dmenu --pre-display-cmd \
-        "echo '%s' | sed 's/-/ /g' | sed 's/\.[^.]*$//' | sed -E 's/\b(\w)/\U\1/g'"
+        | fuzzel --dmenu
 )
 
 if [ -z "$img" ]; then
