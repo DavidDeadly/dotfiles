2a83d140232dd15ea7584767cd2ce59882398753
DavidDeadly <jdrueda513@gmail.com>
2024-09-07 23:47:47 -0500

    nixos/home-manager: define static local ip address & default gateway

--- hosts/davnix/configuration.nix
+++ hosts/davnix/configuration.nix
@@ -17,6 +17,11 @@
 
   networking = {
     hostName = "davnix";
+    interfaces.enp2s0.ipv4.addresses = [{
+      address = "192.168.1.7";
+      prefixLength = 24;
+    }];
+    defaultGateway = "192.168.1.1";
     nameservers = [ "1.1.1.1" "1.0.0.1" ];
     networkmanager.enable = true;
   };
